16f8a7c6e724d53e022997512ccd53ad7d821e73
Jeshant Kalita <jeshant.kalita27@gmail.com>
2021-07-18 21:16:11 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { v4 as uuidv4 } from "uuid";
 
-function App() {
+const App = () => {
+  const [newItem, setNewItem] = useState("");
+  const [todoData, setTodoData] = useState([]);
+  console.log(newItem);
+
+  const addItem = () => {
+    const list = {
+      id: uuidv4(),
+      value: newItem,
+    };
+
+    todoData.push(list);
+
+    setNewItem("");
+  };
+  console.log(todoData);
+
+  const deleteItem = (id) => {
+    const filteredItem = todoData.filter((newItem) => newItem.id !== id);
+    setTodoData(filteredItem);
+    console.log(filteredItem);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="App">
+        <h1>My LIST</h1>
+
+        <div className="container">
+          <h3>Add an Item</h3>
+
+          <br />
+
+          <input
+            type="text"
+            placeholder="Enter the item..."
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+          />
+
+          <button onClick={addItem} className="add-btn">
+            {" "}
+            Add{" "}
+          </button>
+          <br />
+
+          <ul>
+            {todoData.map((item) => {
+              return (
+                <li key={item.id}>
+                  {item.value}
+                  <button
+                    onClick={() => deleteItem(item.id)}
+                    className="delete-btn"
+                  >
+                    Delete
+                  </button>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
